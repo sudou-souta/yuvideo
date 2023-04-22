f57f5ff36180fc60df11f498f5b72082d542e018
sota <sota@sotasudo23.local>
2023-04-23 07:24:39 +0900

    add html

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_templates
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_templates('index.html')
+   return render_template('index.html')
